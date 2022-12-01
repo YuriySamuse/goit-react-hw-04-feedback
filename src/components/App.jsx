0c8c99b8398866c801ae04046fba33842a9f06e3
YuriySamusew <6177385@gmail.com>
2022-12-01 02:45:50 +0200

    handleFeedbck working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 // import { render } from '@testing-library/react';
 import React from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOption';
+import Statistics from './Statistics/Statistics';
 
 class App extends React.Component {
   state = {
@@ -8,23 +9,26 @@ class App extends React.Component {
     neutral: 0,
     bad: 0,
   };
+
+  handleFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
     return (
       <>
         <section>
           <h1>Plese leave feedback</h1>
           <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleFeadback}
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
           />
         </section>
         <section>
           <h2>Statistics</h2>
-          <ul>
-            <li>Good</li>
-            <li>Netural</li>
-            <li>Bad</li>
-          </ul>
+          <Statistics good={good} neutral={neutral} bad={bad} />
         </section>
       </>
     );
