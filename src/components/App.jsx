774d0c6a9f77ebc9698f91b1aca2a863ee9bcc97
YuriySamusew <6177385@gmail.com>
2022-12-02 16:44:28 +0200

    added positive percentage and totla feadback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,21 +14,39 @@ class App extends React.Component {
     this.setState(prevState => ({ [option]: prevState[option] + 1 }));
   };
 
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+    return values.reduce((acc, value) => acc + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = (total, good) => {
+    const percentage = Math.round((good / total) * 100);
+    return percentage;
+  };
+
   render() {
     const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePrcentage = this.countPositiveFeedbackPercentage(total, good);
     const options = Object.keys(this.state);
     return (
       <>
-        <section>
+        <section title="Plese leave feedback">
           <h1>Plese leave feedback</h1>
           <FeedbackOptions
             options={options}
             onLeaveFeedback={this.handleFeedback}
           />
         </section>
-        <section>
+        <section title="Statistics">
           <h2>Statistics</h2>
-          <Statistics good={good} neutral={neutral} bad={bad} />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percentage={positivePrcentage}
+          />
         </section>
       </>
     );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, total, percentage }) => {
   return (
     <>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Postitve Feaback: {percentage}%</p>
     </>
   );
 };
